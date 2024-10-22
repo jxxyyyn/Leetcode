f5351492696b1caebab2981835a1b6ab0685752f
jxxyyyn <142684491+jxxyyyn@users.noreply.github.com>
2024-10-22 19:01:56 +0900

    Time: 26 ms (83.50%), Space: 11.7 MB (97.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,8 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
-
-        if sorted(s) == sorted(t):
+        
+        from collections import Counter
+        count_t = Counter(t)
+        count_s = Counter(s)
+        if count_s == count_t :
             return True
         return False
