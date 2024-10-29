90597e7445893ccd36bfeb406baf5e745dbfa51e
jxxyyyn <142684491+jxxyyyn@users.noreply.github.com>
2024-10-29 19:41:01 +0900

    Time: 12 ms (27.70%), Space: 11.6 MB (56.14%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -7,23 +7,28 @@ class Solution(object):
         
         
         for row in board:
-            nums_in_row = [num for num in row if num != '.']
-            if len(nums_in_row) != len(set(nums_in_row)):
-                return False
+            seen = set()
+            for num in row:
+                if num != '.' and num in seen:
+                    return False
+                seen.add(num)
         
         for col in range(9):
-            nums_in_column = [board[row][col] for row in range(9) if board[row][col] != '.']
-            if len(nums_in_column) != len(set(nums_in_column)):
-                return False
+            seen = set()
+            for row in range(9):
+                num = board[row][col]
+                if num != '.' and num in seen:
+                    return False
+                seen.add(num)
         
         for row_start in range(0, 9, 3):
             for col_start in range(0, 9, 3):
-                grid = set()
+                seen = set()
                 for i in range(3):
                     for j in range(3):
-                        num = board[row_start + i][col_start + j]
-                        if num != '.' and num in grid:
+                        num = board[row_start+i][col_start+j]
+                        if num != '.' and num in seen:
                             return False
-                        grid.add(num)
+                        seen.add(num)
                         
         return True
